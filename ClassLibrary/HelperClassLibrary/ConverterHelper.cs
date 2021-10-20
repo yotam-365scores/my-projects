c46cb82b9833371386ec6a1476b021060a27dff6
yotam-365scores <yotam@365scores.com>
2021-10-20 17:04:04 +0300

    add Converter Helper

---
+++ ClassLibrary/HelperClassLibrary/ConverterHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelperClassLibrary
+{
+	public static class ConverterHelper
+	{
+		public static byte[] GetBytes(string str)
+		{
+			return Encoding.ASCII.GetBytes(str);
+		}
+		public static string FromBytes(byte[] bytes)
+		{
+			return Encoding.ASCII.GetString(bytes);
+		}
+
+
+	}
+}
