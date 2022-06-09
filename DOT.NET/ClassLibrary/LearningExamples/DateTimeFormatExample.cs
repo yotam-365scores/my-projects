5076c5392ef42b6068860f5a16d532328e1e3f9d
yotam-365scores <yotam@365scores.com>
2022-06-09 19:17:13 +0300

    DateTime Formats Example

---
+++ DOT.NET/ClassLibrary/LearningExamples/DateTimeFormatExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningExamples
+{
+	public class DateTimeFormatExample
+	{
+		public static void run()
+		{
+			// create date time 2008-03-09 16:05:07.123
+			DateTime dt = new DateTime(2008, 3, 9, 16, 5, 7, 123);
+			
+			String.Format("{0:y yy yyy yyyy}", dt);  // "8 08 008 2008"   year
+			String.Format("{0:M MM MMM MMMM}", dt);  // "3 03 Mar March"  month
+			String.Format("{0:d dd ddd dddd}", dt);  // "9 09 Sun Sunday" day
+			String.Format("{0:h hh H HH}", dt);  // "4 04 16 16"      hour 12/24
+			String.Format("{0:m mm}", dt);  // "5 05"            minute
+			String.Format("{0:s ss}", dt);  // "7 07"            second
+			String.Format("{0:f ff fff ffff}", dt);  // "1 12 123 1230"   sec.fraction
+			String.Format("{0:F FF FFF FFFF}", dt);  // "1 12 123 123"    without zeroes
+			String.Format("{0:t tt}", dt);  // "P PM"            A.M. or P.M.
+			String.Format("{0:z zz zzz}", dt);  // "-6 -06 -06:00"   time zone
+
+			// month/day numbers without/with leading zeroes
+			String.Format("{0:M/d/yyyy}", dt);  // "3/9/2008"
+			String.Format("{0:MM/dd/yyyy}", dt);  // "03/09/2008"
+
+			// day/month names
+			String.Format("{0:ddd, MMM d, yyyy}", dt);  // "Sun, Mar 9, 2008"
+			String.Format("{0:dddd, MMMM d, yyyy}", dt);  // "Sunday, March 9, 2008"
+
+			// two/four digit year
+			String.Format("{0:MM/dd/yy}", dt);  // "03/09/08"
+			String.Format("{0:MM/dd/yyyy}", dt);  // "03/09/2008"
+		}
+	}
+}
