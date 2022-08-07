9af7925836eeff8ad8a724397dd69581d7953a54
yotam-365scores <yotam@365scores.com>
2022-08-07 18:54:40 +0300

    Observable Class

---
+++ DOT.NET/ClassLibrary/ObservableStart/ObservableClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace ObservableStart
+{
+	public class ObservableClass
+    {
+        public static void StartBackgroundWork()
+        {
+            Console.WriteLine("Shows use of Start to start on a background thread:");
+            var o = Observable
+                .Start(() =>
+            {
+                //This starts on a background thread.
+                Console.WriteLine("From background thread. Does not block main thread.");
+                Console.WriteLine("Calculating...");
+                Thread.Sleep(3000);
+                Console.WriteLine("Background work completed.");
+            })
+                .Finally(() => Console.WriteLine("Main thread completed."));
+            Console.WriteLine("\r\n\t In Main Thread...\r\n");
+            // Wait for completion of background operation.
+            o.Wait();
+            Console.WriteLine("Wait completed");
+        }
+
+        // Synchronous operation
+        public DataType DoLongRunningOperation(string param)
+        {
+            
+        }
+
+        public IObservable<DataType> LongRunningOperationAsync(string param)
+        {
+            return Observable.Create<DataType>(
+                o => Observable.ToAsync<string, DataType>(DoLongRunningOperation)(param).Subscribe(o)
+            );
+        }
+    }
+
+
+    public class TypClass
+    {
+		private int myVar;
+		public int MyProperty
+		{
+			get { return myVar; }
+			set { myVar = value; }
+		}
+
+	}
+    public class ObservableCreateClass
+    {
+
+        // Synchronous operation
+        private static TypClass DoLongRunningOperation(int param)
+        {
+            Console.WriteLine("some Long Running Operation:");
+            return new TypClass() { MyProperty = param };
+        }
+
+        public static IObservable<TypClass> LongRunningOperationAsync()
+        {
+            return Observable.Create<TypClass>(
+                o => Observable
+                .ToAsync<int, TypClass>(DoLongRunningOperation)(7)
+                .Subscribe(o)
+            );
+        }
+
+    }
+
+
+}
